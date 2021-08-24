f49b5ee81a3bd1ce99bfc0609807986cd90f83ac
HyouKa'sh <71031336+HyouKash@users.noreply.github.com>
2021-08-24 23:55:06 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,43 @@
+import win32gui
+import win32con
+import keyboard
+import time
+import pywintypes
+import win32gui, win32com.client
+
+shell = win32com.client.Dispatch("WScript.Shell")
+
+pseudos = []
+hwndd = []
+
+def winEnumHandler(hwnd, ctx):
+    if win32gui.IsWindowVisible(hwnd):
+        if "Dofus" in win32gui.GetWindowText(hwnd):
+            hwndd.append(hwnd)
+            pseudos.append(win32gui.GetWindowText(hwnd))
+
+win32gui.EnumWindows(winEnumHandler, None)
+
+account = 0
+
+print("You're accounts :")
+
+for k in range(len(hwndd)):
+    print(str(k + 1) + ". " + pseudos[k].split(" ")[0])
+
+rep = str(input("Show the initiative like this - account n°1 account n°2 etc.. : "))
+
+initiative = rep.split(" ")
+
+final_initiative = {initiative.index(r): int(r) for r in initiative}
+
+while True:
+    if keyboard.is_pressed('tab'):
+        win32gui.ShowWindow(hwndd[dicalacon[account]-1], win32con.SW_SHOWMAXIMIZED)
+        shell.SendKeys('%')
+        win32gui.SetForegroundWindow(hwndd[dicalacon[account]-1])
+        account += 1
+        if account >= len(hwndd):
+            account = 0
+        time.sleep(0.2)
+            
